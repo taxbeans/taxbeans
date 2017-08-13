5565206b3e438d4151039368cce615e8718e2400
Michael Rogers <mrogers@surfnz.com>
2017-08-14 08:48:30 +1200

    Added invoice and payment classes to track invoices

---
+++ taxbeans-forms/src/main/java/com/github/taxbeans/model/Invoice.java
@@ -0,0 +1,17 @@
+package com.github.taxbeans.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.javamoney.moneta.Money;
+
+public class Invoice {
+	
+	private LocalDate date;
+	
+	private Money amount;
+	
+	private List<Payment> payments;
+	
+
+}
