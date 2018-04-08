eb88044278821320dc19842b5142c30c987f287c
Michael Rogers <michaelrogerscode@gmail.com>
2018-04-09 08:28:42 +1200

    Added ability to use Account as keys to a map

--- taxbeans-forms/src/main/java/com/github/taxbeans/model/Account.java
+++ taxbeans-forms/src/main/java/com/github/taxbeans/model/Account.java
@@ -523,5 +523,30 @@ public class Account {
 		this.guid = UUID.randomUUID().toString();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((guid == null) ? 0 : guid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (guid == null) {
+			if (other.guid != null)
+				return false;
+		} else if (!guid.equals(other.guid))
+			return false;
+		return true;
+	}
+
 
 }
