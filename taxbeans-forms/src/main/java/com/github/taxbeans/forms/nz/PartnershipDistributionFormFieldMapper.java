79ed5790e69abb1a041324e63e75b8b55fb6ce7a
Michael Rogers <mrogers@surfnz.com>
2018-07-05 09:51:31 +1200

    Added 2017 and 2018 forms

--- taxbeans-forms/src/main/java/com/github/taxbeans/forms/nz/PartnershipDistributionFormFieldMapper.java
+++ taxbeans-forms/src/main/java/com/github/taxbeans/forms/nz/PartnershipDistributionFormFieldMapper.java
@@ -3,9 +3,14 @@ package com.github.taxbeans.forms.nz;
 import java.io.InputStream;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PartnershipDistributionFormFieldMapper {
 
 	private static volatile Map<String, String[]> map = null;
+	
+	final static Logger logger = LoggerFactory.getLogger(IR7FieldMapper.class);
 
 	public static String getFieldName(PartnershipDistributionFormFieldEnum fieldName, int year) {
 		if (map == null) {
@@ -19,6 +24,13 @@ public class PartnershipDistributionFormFieldMapper {
 			}
 		}
 		int i = year-2009;
-		return map.get(fieldName.name())[i];
+		String[] strings = map.get(fieldName.name());
+		if (strings.length == 0) {
+			logger.error("No matching fields in IR7 Distribution form {} for: {}", year, fieldName.name());
+		}
+		if (strings.length <= i) {
+			logger.error("No mapping column in IR7 Distribution mapping for year: {}", year);
+		}
+		return strings[i];
 	}
 }
