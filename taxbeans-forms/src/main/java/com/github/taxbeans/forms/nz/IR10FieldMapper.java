fc855b98f74b199a41a83a7643564f3b9f96b9ba
Michael Rogers <mrogers@surfnz.com>
2019-09-22 11:33:13 +1200

    IR10 2018 progress

--- taxbeans-forms/src/main/java/com/github/taxbeans/forms/nz/IR10FieldMapper.java
+++ taxbeans-forms/src/main/java/com/github/taxbeans/forms/nz/IR10FieldMapper.java
@@ -1,6 +1,7 @@
 package com.github.taxbeans.forms.nz;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -24,5 +25,22 @@ public class IR10FieldMapper {
 			}
 		}
 		return map.get(fieldName.name())[year-2009];
+	}
+	
+
+	public static Map<String, String> getPropertyToFieldMap(int year) {
+		Map<String, String> map = new HashMap<String, String>();
+		for (IR10Fields field : IR10Fields.values()) {
+			map.put(field.name(), getFieldName(field, year));
+		}
+		return map;
+	}
+
+	public static Map<String, String> getFieldToPropertyMap(int year) {
+		Map<String, String> map = new HashMap<String, String>();
+		for (IR10Fields field : IR10Fields.values()) {
+			map.put(getFieldName(field, year), field.name());
+		}
+		return map;
 	}
 }
