c77a5da2fdbcb45c5ca23137afb4ab017b58c4bd
michaelrogerscode <michaelrogerscode@gmail.com>
2019-09-23 09:17:55 +1200

    page 1 almost done (do year)

---
+++ taxbeans-forms/src/main/java/com/github/taxbeans/forms/Sum.java
@@ -0,0 +1,12 @@
+package com.github.taxbeans.forms;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Sum {
+
+	String[] value();
+	
+	String[] negate() default {};
+}
