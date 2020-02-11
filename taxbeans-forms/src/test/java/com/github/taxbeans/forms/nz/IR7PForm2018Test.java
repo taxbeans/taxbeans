98b69df8fe9ba6ada13525574b1db8a68b777a33
Michael Rogers <mrogers@surfnz.com>
2020-02-11 17:08:56 +1300

    IR7P for 2019 Completed

--- taxbeans-forms/src/test/java/com/github/taxbeans/forms/nz/IR7PForm2018Test.java
+++ taxbeans-forms/src/test/java/com/github/taxbeans/forms/nz/IR7PForm2018Test.java
@@ -9,7 +9,6 @@ import com.github.taxbeans.forms.common.FormProcessor;
 public class IR7PForm2018Test {
 
 	public static void main(String[] args) {
-		// MJHL IR7
 		IR7PForm2018 bean = new IR7PForm2018();
 		bean.setYearEnded(2018);
 		bean.setIrdNumber("888-888-888");
@@ -24,6 +23,12 @@ public class IR7PForm2018Test {
 		bean.setPartnersOA1(Money.of(new BigDecimal("88.88"), "NZD"));
 		bean.setPartnersRentalIncome1(Money.of(new BigDecimal("88.88"), "NZD"));
 		bean.setPartnersPassiveIncome1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersOtherIncome1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersTotalIncome1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersExtinguishedLosses1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersOverseasTaxPaid1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersImputationCredits1(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersOtherTaxCredits1(Money.of(new BigDecimal("88.88"), "NZD"));
 		bean.setPartnersName2("Jay Johnson");
 		bean.setPartnersIRDNumber2("888-888-882");
 		bean.setPartnersProportion2(new BigDecimal("1"));
@@ -33,6 +38,12 @@ public class IR7PForm2018Test {
 		bean.setPartnersOA2(Money.of(new BigDecimal("8.88"), "NZD"));
 		bean.setPartnersRentalIncome2(Money.of(new BigDecimal("8.88"), "NZD"));
 		bean.setPartnersPassiveIncome2(Money.of(new BigDecimal("8.88"), "NZD"));
+		bean.setPartnersOtherIncome2(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersTotalIncome2(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersExtinguishedLosses2(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersOverseasTaxPaid2(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersImputationCredits2(Money.of(new BigDecimal("88.88"), "NZD"));
+		bean.setPartnersOtherTaxCredits2(Money.of(new BigDecimal("88.88"), "NZD"));
 		
 		FormProcessor.publishDraft(bean, 2018, "ir7p/ir7p-%1$s.pdf", IR7PFieldMapper.getPropertyToFieldMap(2018), 
 				"Example Partnership", "ir7p-%1$s-%2$s-draft.pdf");
